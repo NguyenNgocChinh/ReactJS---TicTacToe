26f147faa8a8bc97f1292803a9f71cabdac0d509
NguyenNgocChinh <chinhnguyen.210399@gmail.com>
2020-09-22 09:49:43 +0700

    Declaring Winner & stopped when have a winner & disable changes when click to square

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ class Board extends React.Component{
   }
   handleClick(i){
     const squares = this.state.squares.slice();
+    if(calculateWinner(this.state.squares) || squares[i])
+      return;
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares : squares,
@@ -27,7 +29,12 @@ class Board extends React.Component{
     return <Square value={this.state.squares[i]} onClick= { ()=> this.handleClick(i) }/>
   }
   render(){
-    const status = 'Next player: ' +  (this.state.xIsNext ? "X" : "O");
+    const winner = calculateWinner(this.state.squares);
+    let status ;
+    if(winner)
+      status = "WINNER is " + winner;
+    else
+      status = 'Next player: ' +  (this.state.xIsNext ? "X" : "O");
     return(
       <div>
         <div className="status">{status}</div>
@@ -66,7 +73,25 @@ class Game extends React.Component{
     );
   }
 }
-
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 export default function App() {
   return (
     <div>
@@ -74,4 +99,4 @@ export default function App() {
     </div>
   );
 }
-// Handle Click Change X-O
+// Declaring Winner & stopped when have a winner & disable changes when click to square
